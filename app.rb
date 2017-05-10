23860cea45854962a9db74ca7600d0ea06b16603
Brian Fan and Chitroopa Manikkavasagam <brian@>
2017-05-10 15:17:13 -0700

    update ingredients_recipes database using Active record update

--- app.rb
+++ app.rb
@@ -18,10 +18,24 @@ post('/recipes/new') do
   name = params.fetch('name')
   instruction = params.fetch('instruction')
   rating = params.fetch('rating')
+  ingredient_ids = params.fetch("ingredient_ids")
   @new_recipe = Recipe.new({:name => name, :instruction => instruction, :rating => rating})
   if @new_recipe.save()
+    @new_recipe.update({:ingredient_ids => ingredient_ids})
     redirect("/")
   else
+    @errors = @new_recipe
+    erb(:recipe_form)
+  end
+end
+
+post('/ingredients/new') do
+  ingredient_name = params.fetch('ingredient-name')
+  @new_ingredient = Ingredient.new({:name => ingredient_name})
+  if @new_ingredient.save()
+    redirect("/recipes/new")
+  else
+    @errors = @new_ingredient
     erb(:recipe_form)
   end
 end
---
+++ public/js/scripts.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+   $('.add-ingredients').click(function(){
+     $('#add-ingredients').show()
+   });
+});
